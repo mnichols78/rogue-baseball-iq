b54995aa7cb4adc758f10cee278bb3ae102f2fad
Michael Nichols <46855597+mnichols78@users.noreply.github.com>
2026-06-21 14:50:43 -0700

    Add Firebase web config

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+window.RBI_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBBog6WZ0ezGscioFNL0Qq6GdsVv7AAO-s",
+  authDomain: "rogue-baseball-iq.firebaseapp.com",
+  projectId: "rogue-baseball-iq",
+  storageBucket: "rogue-baseball-iq.firebasestorage.app",
+  messagingSenderId: "527487483108",
+  appId: "1:527487483108:web:636e1ff784d5d97ace80f1",
+  measurementId: "G-VJ9N7G3T5G"
+};
